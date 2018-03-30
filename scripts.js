878ac5856d0a04d65c1cfa925a8b3f76f36bff0a
Omar Curiel <omarcuriel@Omars-MacBook-Pro.local>
2018-03-29 18:12:07 -0700

    added progress bar

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let player = document.querySelector('.player');
 let video = player.querySelector('.viewer');
 let progress = player.querySelector('.progress');
-let progressBar = player.querySelector('.progres__filled');
+let progressBar = player.querySelector('.progress__filled');
 
 let toggle = player.querySelector('.toggle');
 let skipButtons = player.querySelectorAll('[data-skip]');
@@ -15,11 +15,28 @@ function togglePlay () {
   }
 }
 
+function skip () {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate () {
+  video[this.name] = this.value;
+}
+
 function updateButton () {
   toggle.textContent = this.paused ? '►' : '❚ ❚';
+}
+
+function handleProgress () {
+  let percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
 }
 
 toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
